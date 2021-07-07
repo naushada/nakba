e2937fc0ef5de1cd9bc52d205897c038022d4dee
Mohd Naushad Ahmed <naushad.dln@gmail.com>
2021-07-07 10:55:33 +0530

    updated routing tables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,36 @@ import { RouterModule, Routes } from '@angular/router';
 
 /* Project Specific Component */
 import { LoginComponent } from './FRMWRK/login/login.component';
+import { MenuBarComponent } from './FRMWRK/menu-bar/menu-bar.component';
+import { OperationsDiscrepancyReportComponent } from './REPORTING/operations-discrepancy-report/operations-discrepancy-report.component';
+import { ShipmentHistoryReportComponent } from './REPORTING/shipment-history-report/shipment-history-report.component';
+import { AltRefUpdateComponent } from './SHIPPING/alt-ref-update/alt-ref-update.component';
+import { JobSchedulingComponent } from './SHIPPING/job-scheduling/job-scheduling.component';
+import { ManifestingComponent } from './SHIPPING/manifesting/manifesting.component';
+import { ShipmentPricingComponent } from './SHIPPING/shipment-pricing/shipment-pricing.component';
+import { ShippingComponent } from './SHIPPING/shipping/shipping.component';
+import { SuppliesComponent } from './SHIPPING/supplies/supplies.component';
+import { TrackingEnquiryComponent } from './TRACKING/tracking-enquiry/tracking-enquiry.component';
+import { TrackingEntryComponent } from './TRACKING/tracking-entry/tracking-entry.component';
 
 const routes: Routes = [
+  /* Tracking Submenu Items(s) */
+  { path: 'tracking-enquiry', component: TrackingEnquiryComponent },
+  { path: 'tracking-entry', component: TrackingEntryComponent },
+  /* Shipping Submenu's Item(s) */
+  { path: 'alt-ref-update', component: AltRefUpdateComponent },
+  { path: 'job-scheduling', component: JobSchedulingComponent },
+  { path: 'manifesting', component: ManifestingComponent },
+  { path: 'shipment-pricing', component: ShipmentPricingComponent },
+  { path: 'shipping', component: ShippingComponent },
+  { path: 'supplies', component: SuppliesComponent },
+  /* Reporting Submenu Items(s) */
+  { path: 'operations-discrepancy-report', component: OperationsDiscrepancyReportComponent },
+  { path: 'shipment-history-report', component: ShipmentHistoryReportComponent },
+  /* Landing Page - index/login */
   { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'menu-bar', component: MenuBarComponent },
 ];
 
 @NgModule({
